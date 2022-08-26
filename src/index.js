75c414d9c3fb0dbb56cbf7e9a520ce97aa0aebaf
jakovid <jacobk.dowdle@gmail.com>
2022-08-26 15:15:33 +0800

    refactoring the function to build the sketch pad

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('test bruh');
+import { makeDrawingPad } from "./modules/makeDrawingPad";
+
+(function(){
+    var drawingPadMain = {
+        init: function() {
+            this.cacheBtns();
+            this.bindEvents();
+            makeDrawingPad(4);
+        },
+        cacheBtns: function() {
+            let blackBtn = document.getElementById('blackBtn');
+            let randomBtn = document.getElementById('randomBtn');
+            let eraserBtn = document.getElementById('eraserBtn');
+            let resetBtn = document.getElementById('resetBtn');
+            let sizeSlider = document.getElementById('sizeSlider');
+            let setSizeBtn = document.getElementById('setSizeBtn');
+        },
+        bindEvents: function() {
+            setSizeBtn.addEventListener('click', e => {
+                makeDrawingPad(sizeSlider.value);
+            })
+            console.log(setSizeBtn);
+        }
+    }
+    drawingPadMain.init();
+})();
---
+++ src/modules/makeDrawingPad.js
@@ -0,0 +1,19 @@
+function makeDrawingPad(size) {
+    //clear the pad
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach((div) => div.remove());
+    //build new area
+    let drawingPad = document.getElementById('drawingPad');
+    drawingPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size*size; i++) {
+        let box = document.createElement("div");
+        box.className = 'box';
+        box.innerHTML = '.';
+        // box.addEventListener("mousedown", colorBox);
+        // box.addEventListener("click", colorBox)
+        // box.addEventListener("mouseover", colorBox)
+        drawingPad.append(box);
+    }
+}
+
+export { makeDrawingPad };
